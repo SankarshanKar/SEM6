cd7931ba5d40df0c65a70e3834fd0056cbe0dc14
Sankarshan Kar <sankarshan.kar.sk@gmail.com>
2023-01-13 19:26:50 +0530

    Regular update

--- CloudComputing/Lab1/TaskScheduling.c
+++ CloudComputing/Lab1/TaskScheduling.c
@@ -1,57 +1,54 @@
 #include <stdio.h>
 #include <limits.h>
 
-struct Tasks {
-    int time;
-    int task;
-    int vm;
-};
+int min(int a, int b)
+{
+    if (a < b)
+        return b;
+    else if (b < a)
+        return a;
+}
 
-int taskSchedule(struct Tasks arr[], int size) {
-    int finTask = 0;
-    while (finTask < size) {
-        int smallest = INT_MAX;
-        int temp = 0;
-        for (int i = 0; i < size; i++) {
-            if (arr[i].time < smallest) {
-                smallest = arr[i].time;
-                temp = arr[i].task;
-            }
+void display(int arr[][3])
+{
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d ", arr[i][j]);
         }
-
-        for (int i = 0; i < size; i++) {
-            if (arr[i].task != temp && ) {
-
-            }
-        }
-
-        finTask++;
+        printf("\n");
     }
 }
 
-int main() {
-    int size = 3;
-    struct Tasks arr[size];
+void taskScheduling(int arr[][3], int row, int col)
+{
+    int finTask = 0;
+    while (finTask <= col)
+    {
+        int smallest = INT_MAX;
+        int flag = 0;
+        for (int i = 0; i < col; i++)
+        {
+            if (min(arr[i][0], arr[i][1]) < smallest)
+            {
+                smallest = min(arr[i][0], arr[i][1]);
+                flag = i;
+            }
+        }
+        arr[flag][0] = arr[flag][1] = INT_MAX;
+        finTask++;
+    }
+    display(arr);
+}
 
-    arr[0].time = 100;
-    arr[0].task = 1;
-    arr[0].vm = 2;
+int main()
+{
+    int row = 2, col = 3;
+    int arr[2][3] = {{140, 20, 80}, {100, 100, 70}};
+    display(arr);
 
-    arr[1].time = 20;
-    arr[1].task = 2;
-    arr[1].vm = 1;
-
-    arr[2].time = 60;
-    arr[2].task = 3;
-    arr[2].vm = 1;
+    taskScheduling(arr, row, col);
 
     return 0;
 }
-
-
-/*
-
-140  20   60
-100  100  70
-
-*/
