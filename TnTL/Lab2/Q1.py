3065ad7f8bab081bca90930eb39ce0b3a77e6cc1
Sankarshan Kar <sankarshan.kar.sk@gmail.com>
2023-02-11 23:28:18 +0530

    REgular update

--- TnTL/Lab2/Q1.py
+++ TnTL/Lab2/Q1.py
@@ -1,12 +1,6 @@
 num = input("Enter a number: ")
 
-rev = ""
-i = 1
-
-for i in range(1, len(num) + 1):
-    rev = rev + num[-1 * i]
-
-if (num == rev):
+if (num[0] == num[2]):
     print("The number is a palindrome")
 else:
     print("The number is not a palindrome")
